02ab532279c34694ea33c1fe08ad59143b53d4fa
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-11-15 21:55:54 +0100

    15.11

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const element = React.createElement('a', { href: 'https://zrozumiecreact.pl' }, 'Test');
+
+console.log(<h1>Test 2</h1>);
+console.log(element);
+
+const elementInJSC = <a href='https://zrozumiecreact.pl'>Test 3</a>;
+
+const currentDate = new Date();
+const hours = currentDate.getHours();
+const minutes = currentDate.getMinutes();
+
+console.log(hours, minutes);
+
+const timer = <div>{`${hours}:${minutes}`}</div>;
+
+ReactDOM.createRoot(document.getElementById('root')).render(timer);
