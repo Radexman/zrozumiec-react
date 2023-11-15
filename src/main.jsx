7cd804340b9cc07fb4a70b219c98fc4e9fc9f08b
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-11-15 22:57:26 +0100

    15.11

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const element = React.createElement('a', { href: 'https://zrozumiecreact.pl' }, 'Test');
+const element = <div></div>;
 
-console.log(<h1>Test 2</h1>);
-console.log(element);
-
-const elementInJSC = <a href='https://zrozumiecreact.pl'>Test 3</a>;
-
-const currentDate = new Date();
-const hours = currentDate.getHours();
-const minutes = currentDate.getMinutes();
-
-console.log(hours, minutes);
-
-const timer = <div>{`${hours}:${minutes}`}</div>;
-
-ReactDOM.createRoot(document.getElementById('root')).render(timer);
+ReactDOM.createRoot(document.getElementById('root')).render(element);
